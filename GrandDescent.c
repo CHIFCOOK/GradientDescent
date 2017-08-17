d38bd616ae67c84d9977fd346bf7d21cda8de211
chife <1227090649@qq.com>
2017-08-17 10:00:46 +0800

    add

--- GrandDescent.c
+++ GrandDescent.c
@@ -1,53 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ALPHA 0.003
-#define ROW 3
-#define COLUMN 4
+#define ALPHA 0.031
+#define NUMBER_OF_EXAMPLES 3
+#define NUMBER_OF_VARIABLES 4
 
 typedef struct TrainingSet{
 	int n_examples;
 	int n_variables;
-	float examples[ROW][COLUMN];
-	float results[ROW];
+	double examples[NUMBER_OF_EXAMPLES][NUMBER_OF_VARIABLES];
+	double results[NUMBER_OF_EXAMPLES];
 }Set;
 
-float Hypothesis(float * thea, int row, Set * T);
-float DifferentialOfJ(float * thea,int column, Set * T);
-float * MinimizeJ(float * thea,float alpha, Set * T);
-float * InitThea(int n);
+double Hypothesis(double * thea, int row, Set * T);
+double DifferentialOfJ(double * thea,int column, Set * T);
+double * MinimizeJ(double * thea, double alpha, Set * T);
+double * InitThea(int n);
 
 int main(){
 	Set T = {
-			ROW,
-			COLUMN,
+			NUMBER_OF_EXAMPLES,
+			NUMBER_OF_VARIABLES,
 			{
-				{1.1f,4.4f,6.2f,2.7f},
-				{1.5f,2.3f,3.0f,4.0f},
-				{1.4f,4.3f,2.0f,5.6f}
+				{0.0,3.0,0.0,4.0},
+				{0.0,2.0,3.0,1.0},
+				{0.0,5.0,-9.0,7.0}
 			},
 			{
-				1.2f,
-				3.5f,
-				4.2f
+				7.0,
+				9.0,
+				8.0
 			}
 	};
-	float * thea = InitThea(T.n_variables);
+	double * thea = InitThea(T.n_variables);
+	printf("Caculating...\n");
 	MinimizeJ(thea,ALPHA,&T);
+	
+	for(int i = 0; i<NUMBER_OF_VARIABLES; i++){			
+		printf("thea[%d] = %lf\t",i,thea[i]);
+	}
+	printf("\n");
+
+
+	int n_input = 0;
+	printf("How many examples do you want to predict?\n");
+	scanf(" %d",&n_input);
+
+	double input[n_input][NUMBER_OF_VARIABLES];
+
+	for(int i = 0; i< n_input; i++){
+		input[i][0] = T.examples[i][0];
+		for(int j = 1; j<NUMBER_OF_VARIABLES; j++){
+			printf("Please input example[%d], variable[%d]: ",i+1,j);
+			scanf(" %lf",&input[i][j]);
+		}
+	}
+
+	Set test = {
+				n_input,
+				NUMBER_OF_VARIABLES,
+	};
+	for(int i = 0; i < n_input; i++){
+		for(int j = 0; j<NUMBER_OF_VARIABLES; j++){
+			test.examples[i][j] = input[i][j];
+		}
+	}
+	
+	for(int i = 0; i < n_input; i++){
+		double result = Hypothesis(thea,i,&test);
+		printf("Predict result for example[%d]: %lf\n",i+1,result);
+	}
 	return 0;
 }
 
-float * InitThea(int n){
-	float * thea = (float *)malloc(n*sizeof(float));
+double * InitThea(int n){
+	double * thea = (double*)malloc(n*sizeof(double));
 	for(int i = 0 ; i < n ; i++){
 		thea[i] = 0.0f;
 	}
 	return thea;
 }
 
-float * MinimizeJ(float * thea,float alpha, Set * T){
+double * MinimizeJ(double * thea,double alpha, Set * T){
 	while(1){
-		float * temp = (float *)malloc(T->n_variables*sizeof(float));
+		double * temp = (double*)malloc(T->n_variables*sizeof(double));
 		for(int i = 0; i< T->n_variables; i++){
 			temp[i] = thea[i] - alpha * DifferentialOfJ(thea,i,T);
 		}
@@ -56,15 +92,15 @@ float * MinimizeJ(float * thea,float alpha, Set * T){
 
 		for(int i = 0; i< T->n_variables; i++){
 			thea[i] = temp[i];
-			printf("thea[%d] = %f\t",i,thea[i]);
+			//printf("thea[%d] = %f\t",i,thea[i]);
 		}
-		printf("\n");
+		//printf("\n");
 	}
 	return thea;
 }
 
-float DifferentialOfJ(float * thea,int column, Set * T){
-	float sum = 0.0f;
+double DifferentialOfJ(double * thea,int column, Set * T){
+	double sum = 0.0f;
 	for(int i = 0; i<T->n_examples; i++){
 		sum = sum + 
 				(Hypothesis(thea,i,T)-T->results[i])
@@ -77,8 +113,8 @@ float DifferentialOfJ(float * thea,int column, Set * T){
 	return sum;
 }
 
-float Hypothesis(float * thea, int row, Set * T){
-	float ret = 0.0f;
+double Hypothesis(double * thea, int row, Set * T){
+	double ret = 0.0f;
 	for(int i = 0; i<T->n_variables; i++){
 		ret = ret + ( thea[i] * T->examples[row][i]);
 	}
